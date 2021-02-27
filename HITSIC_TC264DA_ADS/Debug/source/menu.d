f7a7e4ecc69f5fa0e9d27ca6d9f567c48dc33655
ZhenXuLin <1789354292@qq.com>
2021-02-27 16:48:12 +0800

    NEW

--- HITSIC_TC264DA_ADS/Debug/source/menu.d
+++ HITSIC_TC264DA_ADS/Debug/source/menu.d
@@ -2,43 +2,29 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
 menu.o :	../source/menu.c
 ../source/menu.c :
-menu.o :	../SmartCar/SmartCar_Systick.h
-../SmartCar/SmartCar_Systick.h :
-menu.o :	../Configurations/Ifx_Cfg.h
-../Configurations/Ifx_Cfg.h :
-menu.o :	../Libraries/Service/CpuGeneric/_Utilities/Ifx_Assert.h
-../Libraries/Service/CpuGeneric/_Utilities/Ifx_Assert.h :
-menu.o :	../SmartCar/common.h
-../SmartCar/common.h :
-menu.o :	../source/Cpu0_Main.h
-../source/Cpu0_Main.h :
-menu.o :	../Libraries/Service/CpuGeneric/SysSe/Bsp/Bsp.h
-../Libraries/Service/CpuGeneric/SysSe/Bsp/Bsp.h :
-menu.o :	../Libraries/Service/CpuGeneric/_Utilities/Ifx_Assert.h
-../Libraries/Service/CpuGeneric/_Utilities/Ifx_Assert.h :
-menu.o :	../SmartCar/SmartCar_GPIO.h
-../SmartCar/SmartCar_GPIO.h :
-menu.o :	../source/menu.h
-../source/menu.h :
-menu.o :	../SmartCar/SmartCar_Flash.h
-../SmartCar/SmartCar_Flash.h :
-menu.o :	../SmartCar/common.h
-../SmartCar/common.h :
-menu.o :	../Libraries/Service/CpuGeneric/_Utilities/Ifx_Assert.h
-../Libraries/Service/CpuGeneric/_Utilities/Ifx_Assert.h :
-menu.o :	../source/team_ctr.h
-../source/team_ctr.h :
-menu.o :	../SmartCar/common.h
-../SmartCar/common.h :
-menu.o :	../source/image.h
-../source/image.h :
-menu.o :	../SmartCar/common.h
-../SmartCar/common.h :
-menu.o :	../source/team_elec.h
-../source/team_elec.h :
-menu.o :	../SmartCar/common.h
-../SmartCar/common.h :
-menu.o :	../source/image.h
-../source/image.h :
